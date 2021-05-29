a9e28b4908b02c27eb752aaa35868100a1dff484
Zahid Hasan <mdjahidhasan920@gmail.com>
2021-05-29 12:58:30 +0600

    remove dd

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -22,8 +22,6 @@ class MoviesController extends Controller
             ->get('https://api.themoviedb.org/3/movie/now_playing?')
             ->json()['results'];
 
-        dd($latestMovies);
-
         $popularMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/popular')
             ->json()['results'];
