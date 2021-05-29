8bd1eea46514b978641db98948f0724fffaa6b79
Zahid Hasan <mdjahidhasan920@gmail.com>
2021-05-29 12:50:10 +0600

    dd

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -22,6 +22,8 @@ class MoviesController extends Controller
             ->get('https://api.themoviedb.org/3/movie/now_playing?')
             ->json()['results'];
 
+        dd($latestMovies);
+
         $popularMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/popular')
             ->json()['results'];
